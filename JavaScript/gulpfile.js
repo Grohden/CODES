82fd0bf0d990fbceae39885b358c1330a56557f4
Gabriel Rohden <gabriel_rohden@outlook.com>
2017-05-05 10:10:17 -0300

    Corrections

--- JavaScript/gulpfile.js
+++ JavaScript/gulpfile.js
@@ -1,24 +1,25 @@
 process.env.ENV = (function(gulpArguments){
     //Define here the dev tasks
-    let devTasks = [
-        'serve',
-        'watch',
-        
+     let devTasks = [
+        /watch/,
+        /serve/,
+        /run/,
+    
         //Arg to force dev
-        '--dev'
+        /--dev/
     ];
     
     //Arg to force prod (also override the --dev arg)
     let forceProd = gulpArguments.includes('--prod');
-    let contains = gulpArguments.every(function(arg){
-        return devTasks.includes(arg)
+    let contains = gulpArguments.some(function(arg){
+        return devTasks.some(rx => rx.test(arg));
     });
     
-    if(contains && !forceProd){
-        console.log('==== Dev ===');
+   if(contains && !forceProd){
+        console.log('==== Executing tasks in Dev mode ===');
         return 'dev';
     }
     
-    console.log('==== Prod ===');
+    console.log('==== Executing tasks in Prod mode ===');
     return 'prod';
 })(process.argv);
